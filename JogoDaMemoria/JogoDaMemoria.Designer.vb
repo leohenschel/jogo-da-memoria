c2b128f10e0a1a3fb1321e7551c00cf7468df53e
LEONARDO ALVES HENSCHEL <leonardo.ahenschel@senacsp.edu.br>
2019-07-06 13:39:32 -0300

    Senac

---
+++ JogoDaMemoria/JogoDaMemoria.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JogoDaMemoria
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(JogoDaMemoria))
+        Me.Pbox1 = New System.Windows.Forms.PictureBox()
+        Me.Pbox10 = New System.Windows.Forms.PictureBox()
+        Me.Pbox9 = New System.Windows.Forms.PictureBox()
+        Me.Pbox14 = New System.Windows.Forms.PictureBox()
+        Me.Pbox13 = New System.Windows.Forms.PictureBox()
+        Me.Pbox11 = New System.Windows.Forms.PictureBox()
+        Me.Pbox15 = New System.Windows.Forms.PictureBox()
+        Me.Pbox16 = New System.Windows.Forms.PictureBox()
+        Me.Pbox12 = New System.Windows.Forms.PictureBox()
+        Me.Pbox4 = New System.Windows.Forms.PictureBox()
+        Me.Pbox8 = New System.Windows.Forms.PictureBox()
+        Me.Pbox7 = New System.Windows.Forms.PictureBox()
+        Me.Pbox6 = New System.Windows.Forms.PictureBox()
+        Me.Pbox5 = New System.Windows.Forms.PictureBox()
+        Me.Pbox3 = New System.Windows.Forms.PictureBox()
+        Me.Pbox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.Pbox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox16, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pbox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Pbox1
+        '
+        Me.Pbox1.Image = Global.JogoDaMemoria.My.Resources.Resources.capitao2
+        Me.Pbox1.Location = New System.Drawing.Point(150, 12)
+        Me.Pbox1.Name = "Pbox1"
+        Me.Pbox1.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox1.TabIndex = 0
+        Me.Pbox1.TabStop = False
+        Me.Pbox1.Tag = "1"
+        '
+        'Pbox10
+        '
+        Me.Pbox10.Image = Global.JogoDaMemoria.My.Resources.Resources.thor3
+        Me.Pbox10.Location = New System.Drawing.Point(395, 120)
+        Me.Pbox10.Name = "Pbox10"
+        Me.Pbox10.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox10.TabIndex = 1
+        Me.Pbox10.TabStop = False
+        Me.Pbox10.Tag = "4"
+        '
+        'Pbox9
+        '
+        Me.Pbox9.Image = Global.JogoDaMemoria.My.Resources.Resources.hulk
+        Me.Pbox9.Location = New System.Drawing.Point(395, 12)
+        Me.Pbox9.Name = "Pbox9"
+        Me.Pbox9.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox9.TabIndex = 2
+        Me.Pbox9.TabStop = False
+        Me.Pbox9.Tag = "2"
+        '
+        'Pbox14
+        '
+        Me.Pbox14.Image = Global.JogoDaMemoria.My.Resources.Resources.thor3
+        Me.Pbox14.Location = New System.Drawing.Point(515, 120)
+        Me.Pbox14.Name = "Pbox14"
+        Me.Pbox14.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox14.TabIndex = 3
+        Me.Pbox14.TabStop = False
+        Me.Pbox14.Tag = "4"
+        '
+        'Pbox13
+        '
+        Me.Pbox13.Image = Global.JogoDaMemoria.My.Resources.Resources.hulk
+        Me.Pbox13.Location = New System.Drawing.Point(515, 12)
+        Me.Pbox13.Name = "Pbox13"
+        Me.Pbox13.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox13.TabIndex = 4
+        Me.Pbox13.TabStop = False
+        Me.Pbox13.Tag = "2"
+        '
+        'Pbox11
+        '
+        Me.Pbox11.Image = Global.JogoDaMemoria.My.Resources.Resources.iroman
+        Me.Pbox11.Location = New System.Drawing.Point(395, 236)
+        Me.Pbox11.Name = "Pbox11"
+        Me.Pbox11.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox11.TabIndex = 5
+        Me.Pbox11.TabStop = False
+        Me.Pbox11.Tag = "6"
+        '
+        'Pbox15
+        '
+        Me.Pbox15.Image = Global.JogoDaMemoria.My.Resources.Resources.iroman
+        Me.Pbox15.Location = New System.Drawing.Point(515, 236)
+        Me.Pbox15.Name = "Pbox15"
+        Me.Pbox15.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox15.TabIndex = 6
+        Me.Pbox15.TabStop = False
+        Me.Pbox15.Tag = "6"
+        '
+        'Pbox16
+        '
+        Me.Pbox16.Image = Global.JogoDaMemoria.My.Resources.Resources.hulk2
+        Me.Pbox16.Location = New System.Drawing.Point(515, 348)
+        Me.Pbox16.Name = "Pbox16"
+        Me.Pbox16.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox16.TabIndex = 7
+        Me.Pbox16.TabStop = False
+        Me.Pbox16.Tag = "8"
+        '
+        'Pbox12
+        '
+        Me.Pbox12.Image = Global.JogoDaMemoria.My.Resources.Resources.hulk2
+        Me.Pbox12.Location = New System.Drawing.Point(395, 348)
+        Me.Pbox12.Name = "Pbox12"
+        Me.Pbox12.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox12.TabIndex = 8
+        Me.Pbox12.TabStop = False
+        Me.Pbox12.Tag = "8"
+        '
+        'Pbox4
+        '
+        Me.Pbox4.Image = Global.JogoDaMemoria.My.Resources.Resources.capitao3
+        Me.Pbox4.Location = New System.Drawing.Point(150, 348)
+        Me.Pbox4.Name = "Pbox4"
+        Me.Pbox4.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox4.TabIndex = 9
+        Me.Pbox4.TabStop = False
+        Me.Pbox4.Tag = "7"
+        '
+        'Pbox8
+        '
+        Me.Pbox8.Image = Global.JogoDaMemoria.My.Resources.Resources.capitao3
+        Me.Pbox8.Location = New System.Drawing.Point(272, 348)
+        Me.Pbox8.Name = "Pbox8"
+        Me.Pbox8.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox8.TabIndex = 10
+        Me.Pbox8.TabStop = False
+        Me.Pbox8.Tag = "7"
+        '
+        'Pbox7
+        '
+        Me.Pbox7.Image = Global.JogoDaMemoria.My.Resources.Resources.vingadores
+        Me.Pbox7.Location = New System.Drawing.Point(272, 236)
+        Me.Pbox7.Name = "Pbox7"
+        Me.Pbox7.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox7.TabIndex = 11
+        Me.Pbox7.TabStop = False
+        Me.Pbox7.Tag = "5"
+        '
+        'Pbox6
+        '
+        Me.Pbox6.Image = Global.JogoDaMemoria.My.Resources.Resources.iroman2
+        Me.Pbox6.Location = New System.Drawing.Point(272, 120)
+        Me.Pbox6.Name = "Pbox6"
+        Me.Pbox6.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox6.TabIndex = 12
+        Me.Pbox6.TabStop = False
+        Me.Pbox6.Tag = "3"
+        '
+        'Pbox5
+        '
+        Me.Pbox5.Image = Global.JogoDaMemoria.My.Resources.Resources.capitao2
+        Me.Pbox5.Location = New System.Drawing.Point(272, 12)
+        Me.Pbox5.Name = "Pbox5"
+        Me.Pbox5.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox5.TabIndex = 13
+        Me.Pbox5.TabStop = False
+        Me.Pbox5.Tag = "1"
+        '
+        'Pbox3
+        '
+        Me.Pbox3.Image = Global.JogoDaMemoria.My.Resources.Resources.vingadores
+        Me.Pbox3.Location = New System.Drawing.Point(150, 236)
+        Me.Pbox3.Name = "Pbox3"
+        Me.Pbox3.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox3.TabIndex = 14
+        Me.Pbox3.TabStop = False
+        Me.Pbox3.Tag = "5"
+        '
+        'Pbox2
+        '
+        Me.Pbox2.Image = Global.JogoDaMemoria.My.Resources.Resources.iroman2
+        Me.Pbox2.Location = New System.Drawing.Point(150, 120)
+        Me.Pbox2.Name = "Pbox2"
+        Me.Pbox2.Size = New System.Drawing.Size(90, 90)
+        Me.Pbox2.TabIndex = 15
+        Me.Pbox2.TabStop = False
+        Me.Pbox2.Tag = "3"
+        '
+        'JogoDaMemoria
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.JogoDaMemoria.My.Resources.Resources.poster2
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(774, 450)
+        Me.Controls.Add(Me.Pbox2)
+        Me.Controls.Add(Me.Pbox3)
+        Me.Controls.Add(Me.Pbox5)
+        Me.Controls.Add(Me.Pbox6)
+        Me.Controls.Add(Me.Pbox7)
+        Me.Controls.Add(Me.Pbox8)
+        Me.Controls.Add(Me.Pbox4)
+        Me.Controls.Add(Me.Pbox12)
+        Me.Controls.Add(Me.Pbox16)
+        Me.Controls.Add(Me.Pbox15)
+        Me.Controls.Add(Me.Pbox11)
+        Me.Controls.Add(Me.Pbox13)
+        Me.Controls.Add(Me.Pbox14)
+        Me.Controls.Add(Me.Pbox9)
+        Me.Controls.Add(Me.Pbox10)
+        Me.Controls.Add(Me.Pbox1)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "JogoDaMemoria"
+        Me.Text = "Jogo da Memoria"
+        CType(Me.Pbox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox16, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pbox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Pbox1 As PictureBox
+    Friend WithEvents Pbox10 As PictureBox
+    Friend WithEvents Pbox9 As PictureBox
+    Friend WithEvents Pbox14 As PictureBox
+    Friend WithEvents Pbox13 As PictureBox
+    Friend WithEvents Pbox11 As PictureBox
+    Friend WithEvents Pbox15 As PictureBox
+    Friend WithEvents Pbox16 As PictureBox
+    Friend WithEvents Pbox12 As PictureBox
+    Friend WithEvents Pbox4 As PictureBox
+    Friend WithEvents Pbox8 As PictureBox
+    Friend WithEvents Pbox7 As PictureBox
+    Friend WithEvents Pbox6 As PictureBox
+    Friend WithEvents Pbox5 As PictureBox
+    Friend WithEvents Pbox3 As PictureBox
+    Friend WithEvents Pbox2 As PictureBox
+End Class
---
+++ JogoDaMemoria/JogoDaMemoria.vb
@@ -0,0 +1,129 @@
+﻿Imports System.Threading
+
+Public Class JogoDaMemoria
+
+    Dim listaImagens(8) As Image
+    Dim acertos As Integer
+    Dim posicoesEscolhidas As New List(Of String)
+    Dim clicks, tagClique1, tagClique2 As Integer
+    Dim imgClique1, imgClique2 As PictureBox
+
+
+
+    Private Sub Pbox1_Click(sender As Object, e As EventArgs) Handles Pbox1.Click, Pbox2.Click, Pbox3.Click, Pbox4.Click, Pbox5.Click, Pbox6.Click, Pbox7.Click, Pbox8.Click, Pbox9.Click, Pbox10.Click, Pbox11.Click, Pbox12.Click, Pbox13.Click, Pbox14.Click, Pbox15.Click, Pbox16.Click
+        VirarImagem(sender)
+    End Sub
+
+    Private Sub JogoDaMemoria_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NovoJogo()
+
+    End Sub
+
+    Private Sub NovoJogo()
+
+        posicoesEscolhidas.Clear()
+        acertos = 0
+
+
+
+        Dim pontosX() As Integer = {150, 272, 395, 515}
+        Dim pontosY() As Integer = {12, 120, 236, 348}
+
+        'Percorre todas as Pictures BOX
+        For Each pbox As PictureBox In Controls.OfType(Of PictureBox)
+
+            Dim rdn As Random = New Random()
+            Dim x, y As Integer
+            Dim posicao As String
+
+            pbox.Visible = True
+            pbox.Enabled = True
+
+            Do
+
+                x = pontosX(rdn.Next(0, 4))
+                y = pontosY(rdn.Next(0, 4))
+                posicao = x & "x" & y
+
+            Loop Until (posicoesEscolhidas.Contains(posicao) = False)
+
+            pbox.Location = New Point(x, y)
+            posicoesEscolhidas.Add(posicao)
+
+            'Vira as imgs para o verso
+            listaImagens(pbox.Tag) = pbox.Image
+            pbox.Image = My.Resources.verso
+
+
+        Next
+
+
+
+    End Sub
+
+    Sub VirarImagem(pbox As PictureBox)
+
+        clicks = clicks + 1
+        pbox.Image = listaImagens(pbox.Tag)
+        pbox.Enabled = False
+
+
+        If (clicks = 1) Then
+            'primeira img'
+
+            tagClique1 = pbox.Tag
+            imgClique1 = pbox
+
+
+        ElseIf (clicks = 2) Then
+            'segunda img'
+
+            tagClique2 = pbox.Tag
+            imgClique2 = pbox
+
+
+            Thread.Sleep(1000)
+
+
+            Dim acertou As Boolean
+            acertou = (tagClique1 = tagClique2)
+
+            If (acertou) Then
+                'acertou
+
+                imgClique1.Visible = False
+                imgClique2.Visible = False
+                acertos = acertos + 1
+
+                If (acertos = 8) Then
+                    MessageBox.Show("Você Ganhouu!!!")
+                    Dim pergunta As DialogResult = MessageBox.Show("Iniciar um novo jogo?", "O que fazer?", MessageBoxButtons.YesNo)
+                    If (pergunta = vbYes) Then
+                        NovoJogo()
+                    Else
+                        Application.Exit()
+                    End If
+
+
+                End If
+
+
+
+
+            Else
+                'Nao acertou
+
+                imgClique1.Image = My.Resources.verso
+                    imgClique2.Image = My.Resources.verso
+                    imgClique1.Enabled = True
+                    imgClique2.Enabled = True
+
+                End If
+                clicks = 0
+            End If
+
+
+    End Sub
+
+
+End Class
